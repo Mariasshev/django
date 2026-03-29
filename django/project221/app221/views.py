a5c8ec364d3e855a67e0314b505edd645715f570
Mariasshev <sevcenkomaria72@gmail.com>
2026-03-29 14:39:34 +0300

    hw-5

--- django/project221/app221/views.py
+++ django/project221/app221/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.template import loader
 from datetime import datetime
 from .forms import RegistrationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 def hello(request):
     return HttpResponse("Hello World!")
@@ -32,8 +34,9 @@ def register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_data['username']
-            return render(request, 'success.html', {'username': username})
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Акаунт для {username} успішно створено! Тепер ви можете увійти.')
+            return redirect('register') 
     else:
         form = RegistrationForm()
     
